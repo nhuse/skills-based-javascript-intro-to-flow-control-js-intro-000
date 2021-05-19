c8c26e5f2fd8837b23194ee63d1e4c6a477d2010
Nathan Huse <nathan.huse@yahoo.com>
2021-05-19 19:41:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,25 +15,25 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age >= 13 && age <=19) {
-    return "You are a teenager!";
+    return 'You are a teenager!';
   }
   else if(age < 13){
-    return "You are a kid";
+    return 'You are a kid';
   }
   else {
-    return "You are a grownup";
+    return 'You are a grownup';
   }
 }
 
 function ternaryTeenager(age) {
   if (age >= 13 && age <=19) {
-    return "You are a teenager";
+    return 'You are a teenager';
   }
   else if(!(age >= 13 && age <=19)) {
-    return "You are not a teenager";
+    return 'You are not a teenager';
   }
 }
 
 function switchAge(age) {
-
+  
 }
