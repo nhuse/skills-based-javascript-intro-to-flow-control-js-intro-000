7ebc5ecbda39bbbce36dcb38e2e23fe4d7565e0a
Nathan Huse <nathan.huse@yahoo.com>
2021-05-19 19:53:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,10 +31,10 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case  >= 13 && <= 19:
+    case  age >= 13 && age <= 19:
       return 'You are a teenager';
       break;
-    case <= 13 || >= 19:
+    case age <= 13 || age >= 19:
       return 'You have an age';
       break;
   }
